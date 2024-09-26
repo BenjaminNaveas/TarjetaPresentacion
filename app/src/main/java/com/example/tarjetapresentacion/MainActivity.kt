eb3b70d7b9910b4705c0094a7b95378298d8e713
Benjamin Naveas <s.acuna30@alumnos.santotomas.cl>
2024-09-25 21:28:10 -0300

    test prueba 2 cambio de nombre

--- app/src/main/java/com/example/tarjetapresentacion/MainActivity.kt
+++ app/src/main/java/com/example/tarjetapresentacion/MainActivity.kt
@@ -14,7 +14,9 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+
 import androidx.compose.ui.unit.sp
+
 import com.example.tarjetapresentacion.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
